cfd6cdd7d4b706a0d62c80dfda602702c3567eab
Evgeniy Voynov <evgeniy.juke@gmail.com>
2017-08-03 16:16:12 +0300

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
-# Leaflet.Canvas-Icons
-Leaflet plugin for displaying icons on canvas instead of DOM
+# Leaflet.Canvas-Markers
+Leaflet plugin for displaying markers on canvas instead of DOM
+
+## Demo
+
+There's a [demo](eJuke.github.io/Leaflet.Canvas-Markers/examples/index.html) for 10000 points, running on Canvas
+
+## Installation and basic usage
+
+Just download `leaflet.canvas-markers.js` from the `dist` folder and attach it to your project.
+
+```html
+<script src="leaflet.canvas-markers.js"></script>
+```
+
+Now attach layer to map and some markers.
+
+```js
+// Adds a layer
+var ciLayer = L.canvasIconLayer({}).addTo(map);
+
+// Marker definition
+var marker =  L.marker([58.5578, 29.0087], {icon: icon});
+
+// Adding marker to layer
+ciLayer.addMarker(marker);
+```
+
+## Benchmark
+
+Plugin was tested in Google Chrome v59. There is results for 100000 markers:
+
+<table>
+  <thead>
+    <tr>
+      <th>Parameter</th>
+      <th>Default Leaflet Markers</th>
+      <th><b>Leaflet.Canvas-Markers</b></th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Memory used</td>
+      <td>up to 2.8 Gb</td>
+      <td><b>about 300 Mb</b></td>
+    </tr>
+    <tr>
+      <td>First load time</td>
+      <td>160-200 seconds</td>
+      <td><b>7 seconds</b></td>
+    </tr>
+    <tr>
+      <td>Zoom and move time</td>
+      <td>more than 3 minutes</td>
+      <td><b>0.5 seconds</b></td>
+    </tr>
+  </tbody>
+</table>
+
+As you can see DOM operations are slow, so you should use canvas for a large number of markers.
+You can also use L.circleMarker for your points
+
+## Methods 
+
+- **addMarker(marker)**: Adds a marker to the layer
+- **removeMarker(marker, redraw)**: Removes a marker from the layer. Set redraw to `true` if you want to redraw layer after marker remove
+- **redraw()**: Redraws the layer
---
+++ src/leaflet.canvas-markers.js
@@ -0,0 +1,150 @@
+'use strict';
+
+L.CanvasIconLayer = (L.Layer ? L.Layer : L.Class).extend({
+  initialize: function (options) {
+    L.setOptions(this, options);
+  },
+
+  setOptions: function (options) {
+    L.setOptions(this, options);
+    if (this._canvas) {
+      this._updateOptions();
+    }
+    return this.redraw();
+  },
+
+  redraw: function () {
+    this._redraw(true);
+  },
+
+  addMarker: function (marker) {
+    L.Util.stamp(marker);
+
+    if (!this._markers) this._markers = {};
+
+    this._markers[marker._leaflet_id] = marker;
+
+    this._drawMarker(marker);
+  },
+
+  removeMarker: function (marker, redraw) {
+    delete this._markers[marker._leaflet_id];
+    if (redraw) {
+      this._redraw(true);
+    }
+  },
+
+  onAdd: function (map) {
+    this._map = map;
+
+    if (!this._canvas) {
+      this._initCanvas();
+    }
+
+    if (this.options.pane) {
+      this.getPane().appendChild(this._canvas);
+    } else {
+      map._panes.overlayPane.appendChild(this._canvas);
+    }
+
+    map.on('moveend', this._reset, this);
+  },
+
+  onRemove: function (map) {
+    if (this.options.pane) {
+      this.getPane().removeChild(this._canvas);
+    } else {
+      map.getPanes().overlayPane.removeChild(this._canvas);
+    }
+  },
+
+  addTo: function (map) {
+    map.addLayer(this);
+    return this;
+  },
+
+  _drawMarker: function (marker) {
+    var context = this._context;
+
+    var x = this._canvas.width * ((marker._latlng.lng - this._bounds.W) / this._bounds.xDiff),
+        y = this._canvas.height * ((this._bounds.N - marker._latlng.lat) / this._bounds.yDiff);
+
+    if (!marker.canvas_img){
+      marker.canvas_img = new Image();
+      marker.canvas_img.src = marker.options.icon.options.iconUrl;
+      marker.canvas_img.onload = function() {
+        context.drawImage(
+          marker.canvas_img, 
+          x - marker.options.icon.options.iconAnchor[0], 
+          y - marker.options.icon.options.iconAnchor[1]
+        );
+      }
+    } else {
+      context.drawImage(marker.canvas_img, x, y);
+    }
+  },
+
+  _reset: function () {
+    var topLeft = this._map.containerPointToLayerPoint([0, 0]);
+    L.DomUtil.setPosition(this._canvas, topLeft);
+
+    var size = this._map.getSize();
+
+    this._canvas.width = size.x;
+    this._canvas.height = size.y;
+
+    this._redraw();
+  },
+
+  _redraw: function (clear) {
+    if (!this._map) {
+        return;
+    }
+
+    if (clear) {
+      this._context.clearRect(0, 0, this._canvas.width, this._canvas.height);
+    }
+
+    this._saveBounds();
+
+    Object.keys(this._markers).forEach(function(item){
+      this._drawMarker(this._markers[item]);
+    }, this)
+  },
+
+  _saveBounds: function () {
+    var b = this._map.getBounds();
+    this._bounds = {
+      xDiff: b.getEast() - b.getWest(),
+      yDiff: b.getNorth() - b.getSouth(),
+      N: b.getNorth(),
+      S: b.getSouth(),
+      W: b.getWest(),
+      E: b.getEast()
+    }
+  },
+
+  _initCanvas: function () {
+    this._canvas = L.DomUtil.create('canvas', 'leaflet-canvas-icon-layer leaflet-layer');
+    var originProp = L.DomUtil.testProp(['transformOrigin', 'WebkitTransformOrigin', 'msTransformOrigin']);
+    this._canvas.style[originProp] = '50% 50%';
+
+    var size = this._map.getSize();
+    this._canvas.width  = size.x;
+    this._canvas.height = size.y;
+
+    this._saveBounds();
+    this._context = this._canvas.getContext('2d');
+
+    var animated = this._map.options.zoomAnimation && L.Browser.any3d;
+    L.DomUtil.addClass(this._canvas, 'leaflet-zoom-' + (animated ? 'animated' : 'hide'));
+  },
+
+  _updateOptions: function () {
+
+  }
+});
+
+L.canvasIconLayer = function (options) {
+  return new L.CanvasIconLayer(options);
+};
